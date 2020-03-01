79b9bd9eb811c678a37f6d0a177d9d2d5ca71e1d
xaviranik <xaviranik.me@gmail.com>
2020-03-01 12:21:15 +0600

    Initial commit

---
+++ TwoSum.py
@@ -0,0 +1,10 @@
+def two_sum(nums, target):
+    comp = dict()
+
+    for i, n in enumerate(nums):
+        if n in comp:
+            return [comp[n] , i]
+        comp[target - n] = i
+
+a = [3, 2, 4, 1]
+print(two_sum(a, 6))
