5a545f5e433c35ee18905198c550e9f2bd702208
xaviranik <xaviranik.me@gmail.com>
2020-03-14 23:06:33 +0600

    TwoSumII alternative solution

--- TwoSum_II.py
+++ TwoSum_II.py
@@ -9,4 +9,17 @@ def two_sum_sorted(numbers, target):
             return [comp[num], i + 1]
 
 
+def two_sum_sorted_alt(numbers, target):
+    p1 = 0
+    p2 = len(numbers) - 1
+    while p1 <= p2:
+        s = numbers[p1] + numbers[p2]
+        if s < target:
+            p1 += 1
+        elif s > target:
+            p2 -= 1
+        else:
+            return [p1 + 1, p2 + 1]
+
+
 print(two_sum_sorted([1, 4, 6, 10], 14))
